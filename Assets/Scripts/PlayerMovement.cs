0b7796e78aa319f4610133760c118bff7a27954e
DuyguTepebas <100417091+DuyguTepebas@users.noreply.github.com>
2023-05-10 23:27:40 +0300

    left and right move done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,28 +6,72 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   public float moveSpeed = 10f;
+   public float moveSpeed = 1f;
+   public float swerveSpeed = 20f;
+   private Vector2 _deltaTouchStart, _deltaTouchEnd, _dragDelta;
+   private float _slideFactor = 0.1f;
+   [SerializeField] private Vector2 boundaries;
 
-   
-
-
-   IEnumerator MoveForwardRoutine()
+   private void Update()
    {
-      while (true)
+      if (Input.GetMouseButtonDown(0))
       {
-         transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.forward,
-            moveSpeed * Time.deltaTime);
-         yield return null;
+         SetTouchStart();
+      }
+      
+
+      if (Input.GetMouseButton(0))
+      {
+         DragDeltaInput();
       }
    }
 
-   public void StartMoveForwardRoutine()
+   void SetTouchStart()
    {
-      StartCoroutine(nameof(MoveForwardRoutine));
+      _deltaTouchStart = Input.mousePosition;
+      _deltaTouchEnd = Input.mousePosition;
+   }
+   
+   // void SetTouchEnd()
+   // {
+   //    _touchEnd = Input.mousePosition;
+   // }
+
+   void DragDeltaInput()
+   {
+      Debug.Log("deltaDragInput");
+      _deltaTouchStart = Input.mousePosition;
+      _dragDelta.x = (_deltaTouchEnd.x - _deltaTouchStart.x) * _slideFactor;
+      _deltaTouchEnd = _deltaTouchStart;
+      Swerve();
    }
 
+   IEnumerator MoveForwardRoutine()
+      {
+         while (true)
+         {
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.forward,
+               moveSpeed * Time.deltaTime);
+            yield return null;
+         }
+      }
 
+      public void StartMoveForwardRoutine()
+      {
+         StartCoroutine(nameof(MoveForwardRoutine));
+      }
 
+      private float _positionOnX = 0;
+      void Swerve()
+      {
+         Debug.Log("swerve");
+         _positionOnX = transform.position.x;
+         _positionOnX = Mathf.Lerp(_positionOnX, _positionOnX - _dragDelta.x,
+            Time.deltaTime * swerveSpeed);
+         _positionOnX = Mathf.Clamp(_positionOnX, boundaries.x, boundaries.y); //boundrileri tek yerden belirleyebilmek için v2
+         Vector3 newPosOnX = new Vector3(_positionOnX, transform.position.y, transform.position.z);
+         transform.position = newPosOnX;
+      }
 
-
+   
 }//class
