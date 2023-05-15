a72b83b4b5d86c7c93d36222ff4eca97736c44ae
DuyguTepebas <100417091+DuyguTepebas@users.noreply.github.com>
2023-05-15 07:22:28 +0300

    throwing animation done

---
+++ Assets/Scripts/AnchorMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+
+
+
+
+
+
+
+}//class
