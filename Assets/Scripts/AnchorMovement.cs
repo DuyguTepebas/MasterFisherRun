1b1e7114138e33bfeb336512d255fe266d9ce348
DuyguTepebas <100417091+DuyguTepebas@users.noreply.github.com>
2023-05-16 14:58:05 +0300

    anchor movement

--- Assets/Scripts/AnchorMovement.cs
+++ Assets/Scripts/AnchorMovement.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnchorMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed=1f;
+
+    private void FixedUpdate()
+    {
+        StartCoroutine(nameof(MoveForwardRoutine));
+    }
 
 
-
+    IEnumerator MoveForwardRoutine()
+    {
+        while (true)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.forward,
+                speed * Time.deltaTime);
+            transform.Rotate(10f,0f,0f ,Space.Self);
+            yield return null;
+        }
+        
+    }
 
 
 
